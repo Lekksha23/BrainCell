950fec507ce61faa1ef0fcf1f920ec0e84506dfe
Lekksha <bennykorovin@gmail.com>
2022-06-19 21:13:43 +0300

    Two execrises are done

---
+++ BrainTraining/Program.cs
@@ -0,0 +1,14 @@
+﻿using BrainTraining.Exercises;
+
+
+
+//var arr = new List<int> { 2, 4, 5, -3, -2, 0, 0 };
+
+
+//var ex1 = new Exercise1();
+//ex1.FindRatiosOfArrayNumbers(arr);
+
+
+var arr2 = new int[5] { 140537, 24390, 6, 923, 5 };
+var ex2 = new Exercise2();
+ex2.Sum(arr2);

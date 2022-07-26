da6e27cff0c8b40626c3f0f2f024995714741b38
Lekksha <bennykorovin@gmail.com>
2022-07-26 11:25:12 +0300

    Exercise 6 is done

---
+++ BrainTraining/Exercises/Exercise6.cs
@@ -0,0 +1,31 @@
+﻿namespace BrainTraining.Exercises
+{
+    public class Exercise6
+    {
+        /*
+         * The function is expected to return an INTEGER.
+         * Given an array of integers, where all elements but one occur twice, find the unique element.
+         */
+
+        public static int FindUniqueNumber(List<int> list)
+        {
+            var uniqueNumber = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                var count = 0;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (j == i)
+                        continue;
+                    if (list[i] == list[j])
+                        count++;
+                }
+
+                if (count == 0)
+                    uniqueNumber = list[i];
+            }
+            return uniqueNumber;
+        }
+
+    }
+}

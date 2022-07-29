e68cbe3f1415250c7de2d7c819f3dfa3aadbeb33
Lekksha <bennykorovin@gmail.com>
2022-07-29 19:28:08 +0300

    Exercise8 added

---
+++ BrainTraining/Exercises/Exercise8.cs
@@ -0,0 +1,23 @@
+﻿namespace BrainTraining.Exercises
+{
+    public class Exercise8
+    {
+        /*
+        * Given two strings consisting of digits 0 and 1 only, find the XOR of the two strings.
+        */
+
+        public string FindXOR(string s, string t)
+        {
+            string res = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == t[i])
+                    res += '0';
+                else
+                    res += '1';
+            }
+            return res;
+        }
+
+    }
+}
